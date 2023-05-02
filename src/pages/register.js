70723ea427fdfc4f53c3a03a06df9ce8ef9b0030
Parth Barse <parthbarse72@gmail.com>
2023-05-03 02:12:43 +0530

    Index Updated

---
+++ src/pages/register.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import Image from 'next/image'
+import mypic from '../images/bus-img.png'
+
+export default function login() {
+    return (
+        <>
+            <div className="watermark">
+                <div className="watermark__inner">
+                    {/* <div className="watermark__body">Draft</div> */}
+                    <Image
+                        src={mypic}
+                        alt="image"
+                    />
+                </div>
+            </div>
+
+            <div className="container">
+                <h1 className='reg-pg-title'>Register</h1>
+                <hr className='hr-login-pg' />
+
+                <form className='reg-form container'>
+                    <div className="form-row">
+                        <div className="form-group col-md-6 inp-fld-1">
+                            <label for="inputName">Name</label>
+                            <input type="number" className="form-control" id="inputName" placeholder="Name"/>
+                        </div>
+                        <div className="form-group col-md-6 inp-fld-2">
+                            <label for="inputEmail">Email</label>
+                            <input type="email" className="form-control" id="inputEmail" placeholder="Email"/>
+                        </div>
+                    </div>
+                    <div className="form-row">
+                        <div className="form-group col-md-6 inp-fld-1">
+                            <label for="inputNumber">Phone Number</label>
+                            <input type="number" className="form-control" id="inputNumber" placeholder="Phone Number"/>
+                        </div>
+                        <div className="form-group col-md-6 inp-fld-2">
+                            <label for="inputPassword">Password</label>
+                            <input type="password" className="form-control" id="inputPassword" placeholder="Password"/>
+                        </div>
+                    </div>
+                    <div className="form-row">
+                        <div className="form-group col-md-6 inp-fld-2">
+                            <label for="inputRePassword">Re-Password</label>
+                            <input type="password" className="form-control" id="inputRePassword" placeholder="Re-Password"/>
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary login-btn-login-pg rounded-pill">Register</button>
+                </form>
+                <p className="para-text">
+                    Already have an Account ?
+                    <br />
+                    <a href="/register">
+                        Login
+                    </a>
+                </p>
+            </div>
+        </>
+    )
+}
